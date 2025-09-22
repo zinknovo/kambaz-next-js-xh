7f1492b39d538780343d8d86fad607b7c70b52c3
Xintao Hu <hu.xintao@northeastern.edu>
2025-09-22 17:20:58 -0400

    fixed type error

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -3,7 +3,7 @@ import CourseNavigation from "./Navigation";
 export default async function CoursesLayout({
   children,
   params,
-}: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
+}: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
   const { cid } = await params;
   return (
     <div id="wd-courses">
